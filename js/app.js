4c32b6004935a8e01920fac1944159fbf6ee9b43
Francesco-Iannuzzi <iannuzzi.francesco@outlook.com>
2023-03-28 15:01:40 +0200

    add bonus image

--- js/app.js
+++ js/app.js
@@ -1,10 +1,10 @@
 /*
 
 Descrizione:
-Stampare a schermo un messaggio all’interno di un :puntare_a_destra: h1 :puntare_a_sinistra: utilizzando una proprietá nell'oggetto restituito dalla funzione data.
+Stampare a schermo un messaggio all’interno di un h1 utilizzando una proprietá nell'oggetto restituito dalla funzione data. ✓
 
 Bonus:
-Aggiungere alla pagina un’immagine, presa anch’essa da un data.
+Aggiungere alla pagina un’immagine, presa anch’essa da un data. ✓
 
 */
 
@@ -15,7 +15,8 @@ const { createApp } = Vue
 createApp({
     data() {
         return {
-            message: 'ciao',
+            message: 'Hello World',
+            image: 'https://i.pinimg.com/originals/47/83/a6/4783a6a0f05ad0b11dc994aada6defb5.jpg',
         }
     }
 }).mount('#greeting')
